5cedbc06e49a61618cbd6b98b19bcc21bb424595
Rafael Kellermann Streit <rafaelks@me.com>
2018-07-16 15:48:18 -0300

    Revert "[BUG] Reduce the amount of time to ~28 hours instead of the same second"

--- Rocket.Chat/Managers/Model/SubscriptionManager/SubscriptionManager.swift
+++ Rocket.Chat/Managers/Model/SubscriptionManager/SubscriptionManager.swift
@@ -28,8 +28,8 @@ struct SubscriptionManager {
         realm?.refresh()
 
         let client = API.current(realm: realm)?.client(SubscriptionsClient.self)
-        let lastUpdateSubscriptions = auth.lastSubscriptionFetchWithLastMessage?.addingTimeInterval(-100000)
-        let lastUpdateRooms = auth.lastRoomFetchWithLastMessage?.addingTimeInterval(-100000)
+        let lastUpdateSubscriptions = auth.lastSubscriptionFetchWithLastMessage
+        let lastUpdateRooms = auth.lastRoomFetchWithLastMessage
 
         // The call needs to be nested because at the first time the user
         // opens the app we don't have the Subscriptions and the Room object
